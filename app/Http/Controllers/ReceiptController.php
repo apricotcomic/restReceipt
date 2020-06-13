77d25b7fde4068d4b9774b6914fea4815e1d07fe
apricotcomic <apricotcomic@gmail.com>
2020-06-13 11:38:48 +0900

    route整理

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -13,26 +13,6 @@ use App\Http\Requests\ReceiptRequest;
 class ReceiptController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  App\Http\Requests\ReceiptRequest  $request
