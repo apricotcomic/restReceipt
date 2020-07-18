67e3e596452dde6910098e6f0bb7b588bf764b55
apricotcomic <apricotcomic@gmail.com>
2020-07-18 20:42:22 +0900

    コメント化したロジックを削除

--- app/Http/Controllers/ShowReceiptController.php
+++ app/Http/Controllers/ShowReceiptController.php
@@ -16,23 +16,6 @@ class ShowReceiptController extends Controller
     //
     public function display(ShowReceiptRequest $request) {
         //
-        /*
-        $company_id = $request->company_id;
-        $branch_id = $request->branch_id;
-        $terminal_id = $request->terminal_id;
-        $original_receipt_id = $request->original_receipt_id;
-
-        $receipt = \App\Receipt::where([
-            ['company_id', '=', $company_id],
-            ['branch_id', '=', $branch_id],
-            ['terminal_id', '=', $terminal_id],
-            ['original_receipt_id', '=', $original_receipt_id]
-        ])->first();
-
-        $details = \App\receipt_detail::wherereceipt_id($receipt->id)
-            ->orderby('line_no')
-            ->get();
-        */
         $receipt = $request->getReceipt();
         $details = $request->getReceiptDetail($receipt->id);
 
